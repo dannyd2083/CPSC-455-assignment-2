0002bcd649e8e0bd56690ea0cf23c92214096a6e
dannyd2083 <58844507+dannyd2083@users.noreply.github.com>
2021-07-19 10:42:30 -0700

    move file

--- trading-card/src/components/Reducers/index.js
+++
@@ -1,9 +0,0 @@
-import cardManagerReducer from "./cardManager";
-import {combineReducers} from "redux";
-
-
-const allReducers = combineReducers({
-   cardManagerReducer
-});
-
-export default allReducers;
