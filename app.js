498d681cd94fdd5875219ed921183efcb20de34a
dannyd2083 <58844507+dannyd2083@users.noreply.github.com>
2021-07-23 11:43:31 -0700

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/', indexRouter);
 app.use('/api/img',indexRouter);
-//test for github actions 
+//test for github action
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "client", "build")))
   app.get("*", (req, res) => {
