9161945ce3a096a3e80cbb6d8b7f1fd7993cbdfc
dannyd2083 <58844507+dannyd2083@users.noreply.github.com>
2021-07-27 18:20:55 -0700

    try deploy

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <header className='head'>
             <div className= 'head-content'>
-                <h1>TFT Set 5: Reckoning Trading Card</h1>
+                <h1>TFT: Reckoning Trading Card</h1>
             </div>
         </header>
     );
